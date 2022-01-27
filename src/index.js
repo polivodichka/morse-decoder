4f317e494a27c9f2ec778f869327d902f22554e0
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-01-27 10:20:18 +0300

    feat: done :)

--- src/index.js
+++ src/index.js
@@ -38,9 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = [];
+  let answer = '';
+  for (let i = 0; i<expr.length; i+=10){
+    array.push(expr.slice(i, i+10));
+  }
+  
+  for (let i = 0; i<array.length; i++){
+    answer += Morse(array[i])===' '? Morse(array[i]):MORSE_TABLE[Morse(array[i])];
+  }
+  return answer;
 }
 
+function Morse(str){
+  let answer = '';
+  for (let i = 0; i<str.length; i+=2){
+    +str.slice(i, i+2)===0?i+=2:0;
+    +str.slice(i, i+2)===10?answer += '.':0;
+    +str.slice(i, i+2)===11?answer += '-':0;
+    if (str.slice(i, i+2)==='**') return ' '
+  }
+  
+  return answer;
+}
 module.exports = {
     decode
 }
